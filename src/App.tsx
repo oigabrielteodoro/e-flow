700f41ba251e7bf6848e8fc3487644971584320b
Gabriel Teodoro <oi@gabrielteodoro.com>
2021-10-09 04:05:43 -0300

    feat(theme): added global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import { GlobalStyle } from 'ui'
+
 export function App() {
-  return <h1>Vite + TypeScript</h1>
+  return (
+    <>
+      <h1>Vite + TypeScript</h1>
+      <GlobalStyle />
+    </>
+  )
 }
