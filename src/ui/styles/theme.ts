19b81678c44bbd147db1ed8be61047b68b5263d4
Gabriel Teodoro <oi@gabrielteodoro.com>
2021-10-09 04:02:47 -0300

    feat(theme): added colors

---
+++ src/ui/styles/theme.ts
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    white: '#ffffff',
+    blue: {
+      500: '#0047BB',
+    },
+    orange: {
+      500: '#f06400',
+    },
+    red: {
+      500: '#d64b45',
+    },
+    green: '#79c300',
+    gray: {
+      50: '#f5f8fa',
+      500: '#657786',
+      900: '#14171a',
+    },
+  },
+}
