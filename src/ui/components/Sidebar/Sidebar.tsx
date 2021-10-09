b11592dbcd989852a91eacd418a1beeefc3329c0
Gabriel Teodoro <oi@gabrielteodoro.com>
2021-10-09 03:55:42 -0300

    chore(components): add Sidebar component

---
+++ src/ui/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { LOGO } from 'assets'
+
+export function Sidebar() {
+  return (
+    <aside>
+      <img src={LOGO} alt='Monetus Logo' />
+    </aside>
+  )
+}
