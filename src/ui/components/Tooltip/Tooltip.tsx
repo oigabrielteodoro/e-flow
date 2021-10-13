5cac290be26575832063ca8d4cd2f36cc4e11409
Gabriel Teodoro <oi@gabrielteodoro.com>
2021-10-13 02:31:47 -0300

    fix(components): solved typecheck errors

--- src/ui/components/Tooltip/Tooltip.tsx
+++ src/ui/components/Tooltip/Tooltip.tsx
@@ -5,11 +5,12 @@ import * as S from './Tooltip.styled'
 type Props = {
   title: string
   children?: ReactNode
+  variant?: 'default' | 'chart'
 }
 
-export function Tooltip({ title, children }: Props) {
+export function Tooltip({ variant = 'default', title, children }: Props) {
   return (
-    <S.Container>
+    <S.Container variant={variant}>
       <span>{title}</span>
       {children}
     </S.Container>
